637962ff48a7d0cabf0867eed6f1056d89f33b7c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-09 16:46:48 +0100

    going back to use WebComponents

--- client/src/application.ts
+++ client/src/application.ts
@@ -1,24 +1,2 @@
 import "htmx.org";
-
-import HelloWorldScene from "@/scenes/HelloWorldScene.ts";
-import Phaser from "phaser";
-
-const appElement = document.querySelector<HTMLElement>("#app")!;
-const width = document.querySelector<HTMLElement>("body")!.clientWidth;
-const height = document.querySelector<HTMLElement>("body")!.clientHeight;
-
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.WEBGL,
-  parent: appElement,
-  width: width,
-  height: height,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { x: 0, y: 1 }
-    }
-  },
-  scene: [HelloWorldScene]
-}
-
-export default new Phaser.Game(config)
+import "@Components/Board.ts"
---
+++ client/src/components/Board.ts
@@ -0,0 +1,5 @@
+export default class Board extends HTMLElement {
+
+}
+
+customElements.define("board", Board);
