67a22a9d039c2a2c76d27a2e30538f718aa24c14
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-13 19:11:14 +0100

    adding a way to return to the previous magnetized position for the card if the card didn't settle after dispatching the CardMovedEvent to GameCard onStopMovement

--- client/src/components/GameCard.ts
+++ client/src/components/GameCard.ts
@@ -2,13 +2,13 @@ import { CardFamily, CardMagnetizeToEvent, CardMovedEvent, CardNumber, SlotNumbe
 import GameSlot from "@Components/GameSlot.ts";
 
 function getCardFamilyColorClass(family: CardFamily): string {
-  if (family === "swords") return "text-blue-400"
-  if (family === "clubs") return "text-green-400"
-  if (family === "golds") return "text-yellow-400"
-  if (family === "cups") return "text-red-400"
-  if (family === "arcana") return "text-amber-600"
+  if (family === "swords") return "text-blue-400";
+  if (family === "clubs") return "text-green-400";
+  if (family === "golds") return "text-yellow-400";
+  if (family === "cups") return "text-red-400";
+  if (family === "arcana") return "text-amber-600";
 
-  return ""
+  return "";
 }
 
 enum State {
@@ -22,7 +22,7 @@ export default class GameCard extends HTMLElement {
   private initialY: number
 
   private covers: GameCard | GameSlot
-  private coveredBy: GameCard | null
+  // private coveredBy: GameCard | null
 
   private state: State
 
@@ -32,49 +32,47 @@ export default class GameCard extends HTMLElement {
   constructor() {
     super();
 
-    this.state = State.Resting
-    this.initialX = 0
-    this.initialY = 0
-    this.number = parseInt(this.attributes.getNamedItem("number")!.value) as CardNumber
-    this.family = this.attributes.getNamedItem("family")!.value as CardFamily
+    this.state = State.Resting;
+    this.initialX = 0;
+    this.initialY = 0;
+    this.number = parseInt(this.attributes.getNamedItem("number")!.value) as CardNumber;
+    this.family = this.attributes.getNamedItem("family")!.value as CardFamily;
 
-    this.covers = this
-    this.coveredBy = null
+    this.covers = this;
+    // this.coveredBy = null;
   }
 
   connectedCallback(): void {
-    this.addEventListener("mousedown", this.onStartMovement())
-    this.addEventListener("mousemove", this.onMove())
-    this.addEventListener("mouseup", this.onStopMovement())
-    this.addEventListener("mouseleave", this.onStopMovement())
+    this.addEventListener("mousedown", this.onStartMovement());
+    this.addEventListener("mousemove", this.onMove());
+    this.addEventListener("mouseup", this.onStopMovement());
+    this.addEventListener("mouseleave", this.onStopMovement());
 
-    window.addEventListener("card:magnetize:to", this.onMagnetizeTo() as EventListener)
+    window.addEventListener("card:magnetize:to", this.onMagnetizeTo() as EventListener);
 
-    this.classList.toggle(getCardFamilyColorClass(this.family), true)
+    this.classList.toggle(getCardFamilyColorClass(this.family), true);
 
     if (this.attributes.getNamedItem("slot")!.value) {
-      const slotNumber = parseInt(this.attributes.getNamedItem("slot")!.value) as SlotNumber
-      this.covers = document.querySelector<GameSlot>(`#play-area game-slot[number='${slotNumber}']`)!
+      const slotNumber = parseInt(this.attributes.getNamedItem("slot")!.value) as SlotNumber;
+      this.covers = document.querySelector<GameSlot>(`#play-area game-slot[number='${slotNumber}']`)!;
 
-      this.style.top = `${this.covers.getBoundingClientRect().top}px`
-      this.style.left = `${this.covers.getBoundingClientRect().left}px`
+      this.style.top = `${this.covers.getBoundingClientRect().top}px`;
+      this.style.left = `${this.covers.getBoundingClientRect().left}px`;
       // TODO: Attach to slot
     }
   }
 
   disconnectedCallback(): void {
-    this.removeEventListener("mousedown", this.onStartMovement())
-    this.removeEventListener("mousemove", this.onMove())
-    this.removeEventListener("mouseup", this.onStopMovement())
-    this.removeEventListener("mouseleave", this.onStopMovement())
+    this.removeEventListener("mousedown", this.onStartMovement());
+    this.removeEventListener("mousemove", this.onMove());
+    this.removeEventListener("mouseup", this.onStopMovement());
+    this.removeEventListener("mouseleave", this.onStopMovement());
 
-    window.removeEventListener("card:magnetize:to", this.onMagnetizeTo() as EventListener)
+    window.removeEventListener("card:magnetize:to", this.onMagnetizeTo() as EventListener);
   }
 
   private onStartMovement(): (event: MouseEvent) => void {
     return (event: MouseEvent): void => {
-      event.preventDefault();
-
       this.state = State.Moving;
       this.initialX = event.clientX;
       this.initialY = event.clientY;
@@ -83,25 +81,24 @@ export default class GameCard extends HTMLElement {
 
   private onMove(): (event: MouseEvent) => void {
     return (event: MouseEvent): void => {
-      event.preventDefault();
       if (this.state !== State.Moving) return;
 
-      const newX = event.clientX
-      const newY = event.clientY
+      const newX = event.clientX;
+      const newY = event.clientY;
 
-      this.style.top = `${this.offsetTop - (this.initialY - newY)}px`
-      this.style.left = `${this.offsetLeft - (this.initialX - newX)}px`
-      this.initialX = newX
-      this.initialY = newY
+      this.style.top = `${this.offsetTop - (this.initialY - newY)}px`;
+      this.style.left = `${this.offsetLeft - (this.initialX - newX)}px`;
+      this.initialX = newX;
+      this.initialY = newY;
     }
   }
 
-  private onStopMovement(): (event: MouseEvent) => void {
-    return (event: MouseEvent) => {
-      event.preventDefault();
+  private onStopMovement(): () => void {
+    return () => {
       if (this.state !== State.Moving) return;
 
       this.state = State.Settling;
+
       window.dispatchEvent(new CustomEvent<CardMovedEvent>(
         "card:moved",
         {
@@ -112,7 +109,13 @@ export default class GameCard extends HTMLElement {
             }
           }
         })
-      )
+      );
+
+      if (this.state !== State.Settling) return;
+
+      this.state = State.Resting;
+      this.style.top = `${this.covers.getBoundingClientRect().top}px`;
+      this.style.left = `${this.covers.getBoundingClientRect().left}px`;
     }
   }
 
@@ -122,19 +125,18 @@ export default class GameCard extends HTMLElement {
       if (this.number !== event.detail.card.number) return;
       if (this.family !== event.detail.card.family) return;
 
-      this.state = State.Resting
+      this.state = State.Resting;
 
-      const covers: GameSlot | GameCard = event.detail.target
+      const covers: GameSlot | GameCard = event.detail.target;
 
-
-      this.style.top = `${covers.getBoundingClientRect().top}px`
-      this.style.left = `${covers.getBoundingClientRect().left}px`
+      this.style.top = `${covers.getBoundingClientRect().top}px`;
+      this.style.left = `${covers.getBoundingClientRect().left}px`;
 
       // TODO: detach from old
       // TODO: attach to new
-      this.covers = covers
+      this.covers = covers;
     }
   }
 }
 
-customElements.define("game-card", GameCard)
+customElements.define("game-card", GameCard);
