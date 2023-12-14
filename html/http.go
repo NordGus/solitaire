84252c2b7719219909ab0476665054ca008b1177
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-14 21:23:02 +0100

    starting to separating templates and adding additional data to fill the first layer of cards

--- html/http.go
+++ html/http.go
@@ -5,9 +5,17 @@ import (
 	"net/http"
 )
 
+const (
+	cupsCard   = "cups"
+	swordsCard = "swords"
+	clubsCard  = "clubs"
+	goldsCard  = "golds"
+	arcanaCard = "arcana"
+)
+
 type BoardState struct {
 	Slots []Slot
-	Card  Card
+	Cards []Card
 }
 
 type Card struct {
@@ -30,10 +38,21 @@ func (a *App) Routes(router chi.Router) {
 
 		data := BoardState{
 			Slots: slots,
-			Card:  Card{Number: 5, Family: "swords", Slot: 0},
+			Cards: []Card{
+				{Number: 2, Family: cupsCard, Slot: 0},
+				{Number: 15, Family: arcanaCard, Slot: 1},
+				{Number: 5, Family: cupsCard, Slot: 2},
+				{Number: 3, Family: clubsCard, Slot: 3},
+				{Number: 7, Family: arcanaCard, Slot: 4},
+				{Number: 11, Family: swordsCard, Slot: 6},
+				{Number: 13, Family: goldsCard, Slot: 7},
+				{Number: 2, Family: swordsCard, Slot: 8},
+				{Number: 6, Family: swordsCard, Slot: 9},
+				{Number: 8, Family: swordsCard, Slot: 10},
+			},
 		}
 
-		err := a.templates.ExecuteTemplate(writer, "layout.gohtml", data)
+		err := a.templates.ExecuteTemplate(writer, "layout", data)
 		if err != nil {
 			http.Error(writer, err.Error(), http.StatusInternalServerError)
 		}
--- html/main.go
+++ html/main.go
@@ -3,12 +3,32 @@ package html
 import (
 	"embed"
 	"errors"
+	"fmt"
 	"html/template"
 )
 
 var (
 	//go:embed templates
 	templates embed.FS
+
+	funcs = template.FuncMap{
+		"parseCardNumber": func(number uint8, family string) string {
+			if family == "arcana" {
+				return fmt.Sprintf("%d", number)
+			}
+
+			switch number {
+			case 13:
+				return "K"
+			case 12:
+				return "Q"
+			case 11:
+				return "J"
+			default:
+				return fmt.Sprintf("%d", number)
+			}
+		},
+	}
 )
 
 type App struct {
@@ -16,7 +36,17 @@ type App struct {
 }
 
 func New() (*App, error) {
-	tmpls, err := template.ParseFS(templates, "templates/*.gohtml")
+	tmpls, err := template.New("card").Funcs(funcs).ParseFS(templates, "templates/card.gohtml")
+	if err != nil {
+		return nil, errors.Join(errors.New("html: something went wrong parsing templates"), err)
+	}
+
+	tmpls, err = tmpls.New("slot").Funcs(funcs).ParseFS(templates, "templates/slot.gohtml")
+	if err != nil {
+		return nil, errors.Join(errors.New("html: something went wrong parsing templates"), err)
+	}
+
+	tmpls, err = tmpls.New("layout").ParseFS(templates, "templates/layout.gohtml")
 	if err != nil {
 		return nil, errors.Join(errors.New("html: something went wrong parsing templates"), err)
 	}
