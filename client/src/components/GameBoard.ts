57a322d4fd1ce6396a010d781a5506a75af668eb
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-14 21:31:35 +0100

    adding some comments

--- client/src/components/GameBoard.ts
+++ client/src/components/GameBoard.ts
@@ -1,7 +1,15 @@
 
-
+/*
+GameBoard:
+  loadableElementsAmount is this number because the game needs this:
+    - 13 cards for the families cups, golds, clubs and swords.
+    - 22 cards for the arcana family.
+    - 11 play-area slots.
+    - 1 card resting slots for the cards of the families cups, golds, clubs and swords.
+    - 2 card resting slots for the arcana cards. (this to implement the double order resting).
+    - 1 out-of-play-area slot for cards to take cards.
+*/
 export default class GameBoard extends HTMLElement {
-  // loadableElementsAmount is this number because the game needs this
   // private readonly loadableElementsAmount: number = (4*13) + 22 + 11 + 4 + 2 + 1;
   private readonly loadableElementsAmount: number = 21;
   private loadableElementsCount: number;
