779ddf0f13e79a38ba52c1949d97af6e179328c2
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-21 12:55:53 +0100

    moving card color setup to the Card WebComponent

--- client/src/components/Card.ts
+++ client/src/components/Card.ts
@@ -1,17 +1,15 @@
-import {
-  AttachLayerEvent,
-  CardFamily,
-  CardMagnetizeToEvent,
-  CardMovedEvent,
-  CardNumber,
-  StackableEvent
-} from "@/types.ts";
+import { AttachLayerEvent, CardFamily, CardNumber, StackableEvent } from "@/types.ts";
 import CardState from "@Components/card/CardState.ts";
 import LoadedState from "@Components/card/states/LoadedState.ts";
 import RestingSlot from "@Components/RestingSlot.ts";
 import Slot from "@Components/Slot.ts";
 
-function getCardFamilyColorClass(family: CardFamily): string {
+function getCardBackgroundColorClass(family: CardFamily): string {
+  if (family === "arcana") return "bg-gray-900";
+  return "bg-amber-100";
+}
+
+function getCardTextColorClass(family: CardFamily): string {
   if (family === "swords") return "text-blue-700";
   if (family === "clubs") return "text-green-600";
   if (family === "golds") return "text-yellow-600";
@@ -20,6 +18,16 @@ function getCardFamilyColorClass(family: CardFamily): string {
   return "";
 }
 
+function getCardBorderColorClass(family: CardFamily): string {
+  if (family === "swords") return "border-blue-700";
+  if (family === "clubs") return "border-green-600";
+  if (family === "golds") return "border-yellow-600";
+  if (family === "cups") return "border-red-700";
+  if (family === "arcana") return "border-amber-600";
+  return "";
+}
+
+// TODO: Completely migrate to use draggable for the cards
 export default class Card extends HTMLElement {
   static TOP_OFFSET: number = 28;
 
@@ -42,6 +50,10 @@ export default class Card extends HTMLElement {
     this.number = parseInt(this.dataset.number!) as CardNumber;
     this.family = this.dataset.family! as CardFamily;
 
+    this.classList.toggle(getCardBackgroundColorClass(this.family), true);
+    this.classList.toggle(getCardTextColorClass(this.family), true);
+    this.classList.toggle(getCardBorderColorClass(this.family), true);
+
     this.style.top = "5rem";
     this.style.left = `${(this.parentElement!.offsetWidth - this.offsetWidth)/2}px`;
   }
@@ -58,21 +70,14 @@ export default class Card extends HTMLElement {
   set layer(layer: number) { this._layer = layer }
 
   connectedCallback(): void {
-    this.addEventListener("mousedown", this.onStartMovement.bind(this));
-    this.addEventListener("mousemove", this.onMove.bind(this));
-    this.addEventListener("mouseup", this.onStopMovement.bind(this));
-    this.addEventListener("mouseleave", this.onStopMovement.bind(this));
+    this.addEventListener("dragstart", this.onDragStart.bind(this));
+    this.addEventListener("dragend", this.onDragEnd.bind(this));
 
-    document.addEventListener("card:moved", this.onCardMoved.bind(this) as EventListener);
     document.addEventListener("card:moved:settled", this.onCardMovementSettled.bind(this) as EventListener);
     document.addEventListener("stackable:push", this.onPush.bind(this) as EventListener);
     document.addEventListener("stackable:pop", this.onPop.bind(this) as EventListener);
 
-    document.addEventListener("game:elements:attach:layer", this.onAttachLayer.bind(this) as EventListener);
-
-    document.addEventListener("card:magnetize:to", this.onMagnetizeTo.bind(this) as EventListener);
-
-    this.classList.toggle(getCardFamilyColorClass(this.family), true);
+    document.addEventListener("game:elements:attach:layer", this.onAttach.bind(this) as EventListener);
 
     if (this.dataset.slot) {
       this._covers = document.querySelector<Slot>(`#play-area game-slot[data-number='${this.dataset.slot}']`)!;
@@ -92,27 +97,19 @@ export default class Card extends HTMLElement {
   }
 
   disconnectedCallback(): void {
-    this.removeEventListener("mousedown", this.onStartMovement.bind(this));
-    this.removeEventListener("mousemove", this.onMove.bind(this));
-    this.removeEventListener("mouseup", this.onStopMovement.bind(this));
-    this.removeEventListener("mouseleave", this.onStopMovement.bind(this));
+    this.removeEventListener("dragstart", this.onDragStart.bind(this));
+    this.removeEventListener("dragend", this.onDragEnd.bind(this));
 
-    document.removeEventListener("card:moved", this.onCardMoved.bind(this) as EventListener);
     document.removeEventListener("card:moved:settled", this.onCardMovementSettled.bind(this) as EventListener);
     document.removeEventListener("stackable:push", this.onPush.bind(this) as EventListener);
     document.removeEventListener("stackable:pop", this.onPop.bind(this) as EventListener);
 
-    document.removeEventListener("game:elements:attach:layer", this.onAttachLayer.bind(this) as EventListener);
-
-    document.removeEventListener("card:magnetize:to", this.onMagnetizeTo.bind(this) as EventListener);
+    document.removeEventListener("game:elements:attach:layer", this.onAttach.bind(this) as EventListener);
   }
 
-  private onStartMovement(event: MouseEvent): void { this._state.onStartMovement(event) }
-  private onMove(event: MouseEvent): void { this._state.onMove(event) }
-  private onStopMovement(): void { this._state.onStopMovement() }
-  private onMagnetizeTo(event: CustomEvent<CardMagnetizeToEvent>): void { this._state.onMagnetize(event) }
-  private onAttachLayer(event: CustomEvent<AttachLayerEvent>): void { this._state.onAttach(event) }
-  private onCardMoved(event: CustomEvent<CardMovedEvent>): void { this._state.onCardMoved(event) }
+  private onDragStart(event: DragEvent): void { this._state.onDragStart(event) }
+  private onDragEnd(): void { this._state.onDragEnd() }
+  private onAttach(event: CustomEvent<AttachLayerEvent>): void { this._state.onAttach(event) }
   private onCardMovementSettled(): void { this._state.onCardMovementSettled() }
 
   private onPush(event: CustomEvent<StackableEvent>): void {
