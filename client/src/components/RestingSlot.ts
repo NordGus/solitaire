bafcd6ed2573a1c1c9af24b48837db4bf8d81ac9
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-21 19:07:44 +0100

    refactoring RecallCardEvent type to include de caller slot

--- client/src/components/RestingSlot.ts
+++ client/src/components/RestingSlot.ts
@@ -40,7 +40,7 @@ export default class RestingSlot extends HTMLElement {
   private onCardMovementSettled(): void {
     if (this.childElementCount === 0) this.dispatchEvent(new CustomEvent<RecallCardEvent>(
         "recall:card",
-        { detail: { number: this.attachableNumber, family: this.family }}
+        { detail: { number: this.attachableNumber, family: this.family, caller: this }}
       ));
 
     if (this.lastElementChild === null) return;
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,4 +1,5 @@
 import Card from "@Components/Card.ts";
+import RestingSlot from "@Components/RestingSlot.ts";
 
 export type CardFamily = "swords" | "clubs" | "golds" | "cups" | "arcana";
 export type CardNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21;
@@ -7,4 +8,4 @@ export type SlotNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 export type AttachLayerEvent = { layer: number };
 
 export type StackableEvent = { card: Card };
-export type RecallCardEvent = { number: CardNumber, family: CardFamily };
+export type RecallCardEvent = { number: CardNumber, family: CardFamily, caller: RestingSlot };
